df4a316984c0afe15cdc2fe0022239f478c7820c
John Evans <lgastako@gmail.com>
2013-04-30 05:00:57 -0400

    Fixed example to match prismatic's.

--- example1.py
+++ example1.py
@@ -3,7 +3,7 @@ from computator import computate
 GRAPH = {
     "n": lambda xs: len(xs),
     "m": lambda xs, n: sum(xs) / n,
-    "m2": lambda xs, n: sum([x * x for x in xs]),
+    "m2": lambda xs, n: sum([x * x for x in xs]) / float(n),
     "v": lambda m, m2: m2 - (m * m)
 }
 
