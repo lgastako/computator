d954bc933e2ee375fc52a6865bec37f34b9ae69a
John Evans <lgastako@gmail.com>
2013-04-30 05:25:49 -0400

    Add missing-key test.

--- computator.py
+++ computator.py
@@ -6,24 +6,29 @@ class NotReady(Exception):
 class Deadlock(Exception):
     pass
 
-def computate_func(f, vals):
+def computate_func(f, vals, all_names):
     names = inspect.getargspec(f)[0]
     args = []
     for name in names:
+        if name not in all_names:
+            raise KeyError(name)
         if name not in vals:
             raise NotReady
         args.append(vals[name])
     return f(*args)
 
 def computate(graph, **done):
+    all_names = frozenset(graph.keys() + done.keys())
     while len(done) < len(graph):
-        unfinished = frozenset(graph.keys()) - frozenset(done.keys())
-        for name in unfinished:
+        done_names = frozenset(done.keys())
+        remaining = all_names - done_names
+        for name in remaining:
             f = graph[name]
             try:
-                done[name] = computate_func(f, done)
+                done[name] = computate_func(
+                    f, done, all_names)
             except NotReady:
                 pass
-        if len(frozenset(graph.keys()) - frozenset(done.keys())) == len(unfinished):
+        if len(done) == len(done_names):
             raise Deadlock
     return done
--- test_computator.py
+++ test_computator.py
@@ -1,17 +1,17 @@
+import pytest
+
 from computator import computate
 
 class TestPlumbingDocExamples:
+    STATS_GRAPH = {
+        "n": lambda xs: len(xs),
+        "m": lambda xs, n: sum(xs) / n,
+        "m2": lambda xs, n: sum([x * x for x in xs]) / float(n),
+        "v": lambda m, m2: m2 - (m * m)
+    }
 
     def test_stats(self):
-        GRAPH = {
-            "n": lambda xs: len(xs),
-            "m": lambda xs, n: sum(xs) / n,
-            "m2": lambda xs, n:
-                sum([x * x for x in xs]) / float(n),
-            "v": lambda m, m2: m2 - (m * m)
-        }
-
-        results = computate(GRAPH, xs=[1, 2, 3, 6])
+        results = computate(self.STATS_GRAPH, xs=[1, 2, 3, 6])
 
         assert results == {
             "xs" : [1, 2, 3, 6],
@@ -19,3 +19,7 @@ class TestPlumbingDocExamples:
             "m"  : 3,
             "n"  : 4
         }
+
+    def test_missing_key(self):
+        with pytest.raises(KeyError):
+            computate(self.STATS_GRAPH, ys=[1, 2, 3])
